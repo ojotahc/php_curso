bbe8b5f6e192e32676d777af62faf37e9c872e5c
Jotah <ojotahc@gmail.com>
2026-05-01 14:04:53 -0300

    Primeiros scripts em php

---
+++ calculo.php
@@ -0,0 +1,11 @@
+<?php
+
+$val1 = 5;
+$val2 = 2;
+
+$result = $val1 + $val2; // +, -, *, /, **, *** e %
+// não realizar divisão por 0
+// 5_000_000 se quiser trabalhar com números grandes
+echo $result;
+
+var_dump($result);
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+$idade = 18;
+$texto = 'seja bem vindo ao curso';
+$peso = 75.4;
+$aceitouOsTermos = false;
+
+// comentario em linha
+/*
+comentaria em bloco
+*/
+
