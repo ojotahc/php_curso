4cd884e5900ec3e635704bc21997f89a48ca30f7
Jotah <ojotahc@gmail.com>
2026-05-01 22:38:56 -0300

    Comparação em php

---
+++ comparacao.php
@@ -0,0 +1,10 @@
+<?php
+
+$val1 = 5;
+$val2 = 2;
+
+$result = $val1 >= $val2; // ==, <, >, <=, >=, !=, === - três iguais verifica o tipo e se são iguais
+
+// estudar mais à frente sobre coerção de tipos
+
+var_dump($result);
